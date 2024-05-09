1aa66759d8779cf5a064da377c82b4ad1cd67b49
Hwirin-Kim <kimhuirin@gmail.com>
2024-05-09 22:21:51 +0900

    ✨ [FEAT] intersection observer 이용하여 무한스크롤 구현

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -5,23 +5,31 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse>
+  res: NextApiResponse<StoreApiResponse | StoreType[]>
 ) {
-  const { page } = req.query;
-  const pageNumber = typeof page === "string" ? parseInt(page, 10) || 1 : 1;
-  const skipPage = pageNumber - 1;
   const prisma = new PrismaClient();
-  const count = await prisma.store.count();
-  const stores = await prisma.store.findMany({
-    orderBy: { id: "asc" },
-    take: 10,
-    skip: skipPage * 10,
-  });
+  const { page } = req.query;
 
-  res.status(200).json({
-    page: pageNumber,
-    data: stores,
-    totalCount: count,
-    totalPages: Math.ceil(count / 10),
-  });
+  if (page) {
+    const pageNumber = typeof page === "string" ? parseInt(page, 10) || 1 : 1;
+    const skipPage = pageNumber - 1;
+    const count = await prisma.store.count();
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+      take: 10,
+      skip: skipPage * 10,
+    });
+    res.status(200).json({
+      page: pageNumber,
+      data: stores,
+      totalCount: count,
+      totalPages: Math.ceil(count / 10),
+    });
+  } else {
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+    });
+
+    res.status(200).json(stores);
+  }
 }
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,31 +1,78 @@
+import Loader from "@/components/Loader";
 import Loading from "@/components/Loading";
 import Pagination from "@/components/Pagination";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
 import { StoreApiResponse, StoreType } from "@/interface";
 import { markerSelector } from "@/utils/selectMarker";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
-import { useQuery } from "react-query";
+import React, { useCallback, useEffect, useRef } from "react";
+import { useInfiniteQuery, useQuery } from "react-query";
+
+const getStoreData = async (page: string): Promise<StoreApiResponse> => {
+  const { data } = await axios.get(`/api/stores?page=${page}`);
+  return data;
+};
 
 export default function StoreListPage() {
   const router = useRouter();
+  const ref = useRef<HTMLDivElement | null>(null);
+  const observerRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!observerRef?.isIntersecting;
 
+  console.log(isPageEnd);
   const { page = "1" } = router.query;
   console.log(page);
 
+  // const {
+  //   isLoading,
+  //   isError,
+  //   data: stores,
+  // } = useQuery({
+  //   queryKey: ["stores", page],
+  //   queryFn: async (): Promise<StoreApiResponse> => {
+  //     const { data } = await axios.get(`/api/stores?page=${page}`);
+  //     return data;
+  //   },
+  // });
+
   const {
+    data,
+    isFetching,
     isLoading,
     isError,
-    data: stores,
-  } = useQuery({
-    queryKey: ["stores", page],
-    queryFn: async (): Promise<StoreApiResponse> => {
-      const { data } = await axios.get(`/api/stores?page=${page}`);
-      return data;
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["stores"],
+    queryFn: ({ pageParam = 1 }) => getStoreData(pageParam),
+    getNextPageParam: (lastPage) => {
+      if (lastPage.totalPages > lastPage.page) return lastPage.page + 1;
+      else return undefined;
     },
   });
+
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.error("error fetching next page", res.error);
+    }
+  }, [fetchNextPage]);
+
+  console.log(data);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(() => {
+        fetchNext();
+      }, 500);
+    }
+    return () => clearTimeout(timerId);
+  }, [fetchNext, isPageEnd, hasNextPage]);
 
   if (isError) {
     return (
@@ -41,46 +88,55 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.data.map((store, index) => {
-            return (
-              <li className="flex justify-between gap-x-6 py-5" key={index}>
-                <div className="flex gap-x-4">
-                  <Image
-                    src={markerSelector(store.category!)}
-                    width={48}
-                    height={48}
-                    alt="icon_image"
-                  />
-                  <div>
-                    <div className="text-sm font-semibold leading-6  text-gray-900">
-                      {store.name}
+          data?.pages.map((page, i) => (
+            <React.Fragment key={i}>
+              {page.data.map((store, index) => {
+                return (
+                  <li className="flex justify-between gap-x-6 py-5" key={index}>
+                    <div className="flex gap-x-4">
+                      <Image
+                        src={markerSelector(store.category!)}
+                        width={48}
+                        height={48}
+                        alt="icon_image"
+                      />
+                      <div>
+                        <div className="text-sm font-semibold leading-6  text-gray-900">
+                          {store.name}
+                        </div>
+                        <div className="mt-1 truncate text-sm font-semibold leading-5  text-gray-500">
+                          {store.name}
+                        </div>
+                      </div>
                     </div>
-                    <div className="mt-1 truncate text-sm font-semibold leading-5  text-gray-500">
-                      {store.name}
+                    <div className="hidden sm:flex sm:flex-col sm:items-end">
+                      <div className="text-sm font-semibold leading-6  text-gray-900">
+                        {store.address}
+                      </div>
+                      <div className="mt-1 truncate text-sm font-semibold leading-5  text-gray-500">
+                        {store.phone || "번호없음"} | {store.foodCertifyName}
+                        {store.category}
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div className="hidden sm:flex sm:flex-col sm:items-end">
-                  <div className="text-sm font-semibold leading-6  text-gray-900">
-                    {store.address}
-                  </div>
-                  <div className="mt-1 truncate text-sm font-semibold leading-5  text-gray-500">
-                    {store.phone || "번호없음"} | {store.foodCertifyName} |{" "}
-                    {store.category}
-                  </div>
-                </div>
-              </li>
-            );
-          })
+                  </li>
+                );
+              })}
+            </React.Fragment>
+          ))
         )}
       </ul>
-      {stores?.totalPages && (
+
+      {/* 이건 페이지네이션.. 무한스크롤 구현하고 필요없어짐 */}
+      {/* {stores?.totalPages && (
         <Pagination
           totalPages={stores.totalPages}
           currentPage={parseInt(page as string)}
           pathname="stores"
         />
-      )}
+      )} */}
+      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+      <div className="w-full touch-none h-10 mb-10" ref={ref} />
+      <button onClick={() => fetchNextPage()}>다음페이지</button>
     </div>
   );
 }
